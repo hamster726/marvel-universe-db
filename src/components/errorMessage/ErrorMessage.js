842f20a0a37be7b5bcc0a4ac0e21eadcf573eb8d
Hamster726 <yevhen.shaparenko@gmail.com>
2021-10-26 13:26:12 +0300

    added error message

--- src/components/errorMessage/ErrorMessage.js
+++ src/components/errorMessage/ErrorMessage.js
@@ -1,7 +1,7 @@
 import ErrorIcon from './error-icon.svg';
 
 const ErrorMessage = () => {
-  return (ErrorIcon)
+  return (<img src={ErrorIcon} style={{width: '250px', height: '250px', margin: '0 auto'}}/>)
 }
 
 export default ErrorMessage;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -3,6 +3,7 @@ import mjolnir from '../../resources/img/mjolnir.png';
 import {Component} from "react";
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
 class RandomChar extends Component {
   state = {
@@ -44,12 +45,18 @@ class RandomChar extends Component {
 
 
   render() {
-    const {char,loading} = this.state;
+    const {char,loading, error} = this.state;
+    const errorMessage = error ? <ErrorMessage/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = !(loading || error) ? <View char={char}/> : null;
+
 
     return (
       <>
         <div className="randomchar">
-          {loading ? <Spinner/> : <View char={char}/>}
+          {errorMessage}
+          {spinner}
+          {content}
           <div className="randomchar__static">
             <p className="randomchar__title">
               Random character for today!<br/>
